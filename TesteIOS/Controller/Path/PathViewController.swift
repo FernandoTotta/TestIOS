edfce3e62b3c06dc4ea20bbb430eb4024f865d14
Fernando Vieira Cardoso <f-vieira1@hotmail.com>
2021-04-17 19:24:42 -0300

    Finalizado ciclo do Path

--- TesteIOS/Controller/Path/PathViewController.swift
+++ TesteIOS/Controller/Path/PathViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class PathViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var pathTextField: UITextField!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        pathTextField.delegate = self
         self.view.backgroundColor = .brown
     }
     
+    @IBAction func VerifyPath(_ sender: UIButton) {
+        guard let pathTextField = pathTextField.text else { return }
+        let path = Path(currentPath: "a/b/c/d")
+        resultLabel.text = path.cd(path: pathTextField)
+        resultLabel.isHidden = false
+    }
+    
+}
+
+extension PathViewController: UITextFieldDelegate {
+    
+    func hideKeyboard() {
+        pathTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hideKeyboard()
+        return true
+    }
 
 }
+
