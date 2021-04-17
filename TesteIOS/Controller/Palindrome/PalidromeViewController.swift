fb0cbe892a434e235ca146d7e1132698174afc80
Fernando Vieira Cardoso <f-vieira1@hotmail.com>
2021-04-17 19:00:00 -0300

    Finalizado o ciclodo palindromo

--- TesteIOS/Controller/Palindrome/PalidromeViewController.swift
+++ TesteIOS/Controller/Palindrome/PalidromeViewController.swift
@@ -9,8 +9,42 @@ import UIKit
 
 class PalidromeViewController: UIViewController {
 
+    @IBOutlet weak var palidromoTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .green
+        palidromoTextField.delegate = self
+        // Do any additional setup after loading the view.
     }
+    
+
+    @IBAction func verifyPalindrome(_ sender: UIButton) {
+        guard let palindromo = palidromoTextField.text else { return }
+        let palindrome = Palindrome.isPalindrome(word: palindromo)
+        
+        if palindrome{
+            var text = ""
+            text = "\(palindromo) é um palindromo"
+            resultLabel.isHidden = false
+            resultLabel.text = text
+        }else {
+            resultLabel.isHidden = false
+            resultLabel.text = "Não é palindromo"
+        }
+    }
+    
+}
+extension PalidromeViewController: UITextFieldDelegate {
+    
+    func hideKeyboard() {
+        palidromoTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hideKeyboard()
+        return true
+    }
+
 }
