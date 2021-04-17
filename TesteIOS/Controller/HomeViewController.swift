ea1aa46e6d758a1a56638b0fe3a1c2f77e7b745e
Fernando Vieira Cardoso <f-vieira1@hotmail.com>
2021-04-17 13:59:23 -0300

    Configuracoes iniciais

--- TesteIOS/Controller/HomeViewController.swift
+++ TesteIOS/Controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  TesteIOS
 //
 //  Created by Fernando Totta on 17/04/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
